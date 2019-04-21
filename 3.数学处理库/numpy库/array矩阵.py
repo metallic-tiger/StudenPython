e2a9790886d66102baacbd4f463930faea6f9a22
matallic-tiger <511032766@qq.com>
2019-04-21 23:21:07 +0800

    提交numpy中array的内容

--- 3.数学处理库/numpy库/array矩阵.py
+++ 3.数学处理库/numpy库/array矩阵.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-a = np.array([1,2,3])
-print (a)
+#numpy.array(object, dtype = None, copy = True, order = None, subok = False, ndmin = 0)
+#   object  数组(或者元祖)，或着是 嵌套数组(或者嵌套元祖)
+#   dtype   元素的数据类型  默认不限定
+#   copy    是否可以进行复制    【还没有试】
+#   order   
+# #
+A=np.array([(1,1),(1,1),(1,0)])
+B=np.array([(1,1),(1,1),(1)])
+#与MATLAB不同 使用‘ ’不能分割元素
+#矩阵中的行列必须对齐
+#即：每个内涵的列表（或者元组）中的 长度必须相同
+#   否者则会当做 以列表作为元素的 一维矩阵
+# 
+# #
+
+#对齐的矩阵
+print("我是对齐的矩阵\n",A)
+#没有对齐的一维矩阵
+print("我是对齐的矩阵\n",B)
